3d5f576d8dbe584892b928119a18bcba3daa679f
ElderGr <gbielrs@gmail.com>
2021-01-26 07:37:55 -0300

    feat: criado repositorio de userTokens, controllers para reset e forgot password junto com migrations para userTokens

---
+++ src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from "express";
+import { container } from "tsyringe";
+
+import SendForgotPasswordEmailService from "@modules/users/services/SendForgotPasswordEmailService";
+
+export default class ForgotPasswordController {
+    public async create(req: Request, res: Response): Promise<Response>{
+        const {email} = req.body;
+
+        const sendForgotPasswordEmail = container.resolve(SendForgotPasswordEmailService)
+
+        await sendForgotPasswordEmail.execute({
+            email,
+        });
+
+
+        return res.status(204).json();
+    }
+}
