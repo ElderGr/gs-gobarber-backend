eadcea232133802d01c575c6bce83f9a52ce98ee
ElderGr <gbielrs@gmail.com>
2021-03-25 18:19:17 -0300

    feat: adicionado variaveis de ambiente na aplicacao

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default{
     jwt:{
-        secret: 'cb4f0d246dc8156e6c20d755123c4cd8',
+        secret: process.env.APP_SERCRET,
         expiresIn: '1d',
     }
 }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import 'dotenv/config';
+
 import { errors } from 'celebrate';
 import express, {Request, Response, NextFunction} from 'express';
 import cors from 'cors';
